8316ecd316c87933c5429b5a98c4a6219064afd8
Hasan Naseem <hn92@live.com>
2018-05-12 00:19:43 -0400

    successfully displaying data from uber API and lyft API on the show page

---
+++ app/controllers/price_controller.rb
@@ -0,0 +1,45 @@
+require 'net/http'
+require 'uri'
+
+class PriceController < ApplicationController
+  def show
+
+  # API calls to uber for price estimation
+
+  uber_url = "https://api.uber.com/v1.2/estimates/price?start_latitude=37.7752315&start_longitude=-122.418075&end_latitude=37.7752415&end_longitude=-122.518075"
+  uber_uri = URI.parse(uber_url)
+  uber_request = Net::HTTP::Get.new(uber_uri)
+  uber_request.content_type = "application/json"
+  uber_request["Authorization"] = "Token AZwX8nXKwQu_44cPXm3vIXG-_ZBhBe9xI5Iq4lkS"
+  uber_request["Accept-Language"] = "en_US"
+
+  req_options = {
+    use_ssl: uber_uri.scheme == "https",
+  }
+
+  uber_response = Net::HTTP.start(uber_uri.hostname, uber_uri.port, req_options) do |http|
+    http.request(uber_request)
+    end
+
+
+    @uber_data = JSON.parse(uber_response.body)
+
+    # API calls to lyft for price estimates
+
+    lyft_uri = URI.parse("https://api.lyft.com/v1/cost?start_lat=37.7763&start_lng=-122.3918&end_lat=37.7972&end_lng=-122.4533")
+    lyft_request = Net::HTTP::Get.new(lyft_uri)
+    lyft_request["Authorization"] = "Basic aG45MkBsaXZlLmNvbTpNaWNybzE5OTI="
+
+    req_options = {
+      use_ssl: lyft_uri.scheme == "https",
+    }
+
+    lyft_response = Net::HTTP.start(lyft_uri.hostname, lyft_uri.port, req_options) do |http|
+      http.request(lyft_request)
+    end
+
+    @lyft_data = JSON.parse(lyft_response.body)
+
+
+  end
+end
