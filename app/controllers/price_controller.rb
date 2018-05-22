476e76f4927fabe86a0816daa04086fe0de83f6f
Hasan Naseem <hn92@live.com>
2018-05-22 11:16:56 -0400

    api refactoring part1

--- app/controllers/price_controller.rb
+++ app/controllers/price_controller.rb
@@ -44,16 +44,21 @@ class PriceController < ApplicationController
 
 
     uber_data = JSON.parse(uber_response.body)
+
       if uber_data["prices"]
           uber_data["prices"].each do |option|
+
             hash = {}
+
             hash["type"]      = option["localized_display_name"]
             hash["distance"]  = option["distance"]
             fare              = option["estimate"].split("$")[1].split('-') if option["estimate"].split("$")[1]
             hash["fare"]      = (fare[0].to_i + fare[1].to_i) / 2 if fare
             hash["currency"]  = option["currency_code"]
             hash["duration"]  = option["duration"]
+
             @data[:uber].push(hash)
+
         end
       else
         @data[:uber][0] = "UNVALID PARAMS"
@@ -80,13 +85,17 @@ class PriceController < ApplicationController
 
     if lyft_data["cost_estimates"]
       lyft_data["cost_estimates"].each do |option|
+
         hash = {}
+
         hash["type"]      = option["display_name"]
         hash["distance"]  = option["estimated_distance_miles"]
         hash["fare"]      = (option["estimated_cost_cents_max"] + option["estimated_cost_cents_min"])/200
         hash["currency"]  = option["currency"]
         hash["duration"]  = option["estimated_duration_seconds"]
+
         @data[:lyft].push(hash)
+        
       end
     else
       @data[:lyft][0] = "INVALID PARAMS"
