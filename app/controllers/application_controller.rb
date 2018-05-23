87e5d87b115c6b23e1ab2c290f46d5f5129b8b3c
donrestarone <shashikejayatunge@gmail.com>
2018-05-23 14:01:56 -0400

    import methods from test repo to main app's application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,10 +56,88 @@ class ApplicationController < ActionController::Base
         end
 
         # http://localhost:3000/price/show.json?sl=43.6549496&slon=-79.3759270&&el=43.6420997&&elon=-79.4402033
+  end
+  def sort_uber_prices(uber_data)
+    uber_prices = Array.new
+    uber_data.each do |fare|
+      if fare[:fare] != nil
+        uber_prices.push(fare[:fare])
+      end
+    end 
+    return uber_prices.sort{|x,y| x <=> y}
+  end
 
-      @data
-
+  def sort_lyft_prices(lyft_data) 
+    lyft_prices = Array.new
+    lyft_data.each do |fare|
+      if fare[:fare] != nil
+        lyft_prices.push(fare[:fare])
+      end
     end
+    return lyft_prices.sort{|x,y| x <=> y}
+  end
+
+  def uber_lyft_data_sorter(data)
+    uber_prices = sort_uber_prices(data[:uber])
+
+    lyft_prices = sort_lyft_prices(data[:lyft])
+
+    sorted_data = Hash.new
+    
+    uber = Array.new
+    lyft = Array.new
+
+    uber_prices.each do |price|
+      data[:uber].each do |fare|
+        if fare[:type] == "POOL"
+          fare[:type] = "UberPool"
+        end
+        if fare[:fare] == price
+          
+          ride = Hash.new
+          ride[:type] = fare[:type]
+          ride[:price] = fare[:fare]
+          ride[:distance] = fare[:distance]
+          ride[:currency] = fare[:currency]
+          ride[:duration] = fare[:duration]
+
+          uber.push(ride)
+        end
+      end
+    end
+    uber = return_uber_data(uber)
+
+    lyft_prices.each do |price|
+      data[:lyft].each do |fare|
+        if fare[:fare] == price
+          ride = Hash.new
+          ride[:type] = fare[:type]
+          ride[:price] = fare[:fare]
+          ride[:distance] = fare[:distance]
+          ride[:currency] = fare[:currency]
+          ride[:duration] = fare[:duration]
+
+          lyft.push(ride)
+        end
+      end
+    end
+    lyft = return_lyft_data(lyft)
+    sorted_data = Hash.new
+    sorted_data[:uber] = uber
+    sorted_data[:lyft] = lyft
+    
+    return sorted_data
+
+  end
+
+
+  def return_uber_data(sorted_uber_data)
+    return sorted_uber_data.uniq
+  end
+
+  def return_lyft_data(sorted_lyft_data)
+    return sorted_lyft_data.uniq
+  end
 
   def current_user
   	if session[:user_id]
