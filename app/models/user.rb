5128a7d7b9dce8cd1ea19dcbc841dec9a5743944
donrestarone <shashikejayatunge@gmail.com>
2018-05-22 21:32:11 -0400

    add basic validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
 	has_many :rides
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, presence: true
 end
