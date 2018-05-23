40edeb8128f036899857079871bd3f9ce41fa600
Karen <kjgamis@live.ca>
2018-05-23 14:15:29 -0400

    add indentation, classes, css styling for readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-  
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  #routes from before 
-  get 'price/show'
-  get '/price/:id', to: 'price#show'
-  get '/google', to: 'google#index'
-  get '/map', to: 'google#map'
+
   root 'google#index'
-  get '/position', to: 'google#position'
-  root 'google#map'
-  ###
+
+  #routes from before
+  get '/google', to: 'google#index'
 
   resource :user, :except => [:destroy] do
   	resources :rides
