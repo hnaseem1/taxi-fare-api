626525d84055fe206c565405414b4c9aaa369a82
donrestarone <shashikejayatunge@gmail.com>
2018-05-24 17:32:25 -0400

    generate reset controller for handling incoming reset request, and post request for changing user password

---
+++ app/controllers/reset_controller.rb
@@ -0,0 +1,10 @@
+class ResetController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
 
+  get 'reset/show'
+  get 'reset/new'
+  get 'reset/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'google#index'
