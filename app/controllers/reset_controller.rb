b3caf0b3658eb5f47c21c0d7cb681ad5ab4363a9
donrestarone <shashikejayatunge@gmail.com>
2018-05-24 18:40:47 -0400

    add password reset email view to action mailer method

--- app/controllers/reset_controller.rb
+++ app/controllers/reset_controller.rb
@@ -9,6 +9,19 @@ class ResetController < ApplicationController
 
   def create
   	@reset = Reset.new
-  	@email = 
+  	@email = params[:reset][:email]
+  	@user = User.find_by(email: @email)
+
+  	if @user
+  		@reset_hash = SecureRandom.hex(10)
+  		@reset.user_id = @user.id
+  		@reset.token = @reset_hash
+
+  		if @reset.save
+  			#@reset_url = 
+  			token = @reset.token
+  			#UserMailer.password_reset_email(@user, token, @reset_url)
+  		end
+  	end
   end
 end
