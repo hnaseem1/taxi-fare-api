69d8c9f885a883d4518404eb35e17ee454ae93e6
donrestarone <shashikejayatunge@gmail.com>
2018-05-24 11:16:12 -0400

    add errors to sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
   		redirect_to user_path
   	else 
-  		redirect_to new_user_path
+  		render :new 
   	end
   end
 
