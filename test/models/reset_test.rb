6b8509d2d999dc5e6d55c463b7258a1abf12a5bb
donrestarone <shashikejayatunge@gmail.com>
2018-05-27 19:30:24 -0400

    add test to resettest for cchecking that a user without token cannot reset password

--- test/models/reset_test.rb
+++ test/models/reset_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ResetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test 'user without token cannot reset password' do
+		user = User.new
+		user.first_name = 'test'
+		user.last_name = 'tester'
+		user.email = 'testemail'
+		user.password = 'pass'
+		user.password_confirmation = 'pass'
+		user.match = true
+		user.save
+
+		reset = Reset.new(user_id: 2, token: "sfhgjfdjf8")
+		reset.save
+		assert_not_equal(user.id, reset.user_id)
+	end
 end
