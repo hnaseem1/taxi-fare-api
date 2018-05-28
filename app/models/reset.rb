b48504085c69a62a33fd6acbe76dcdc4bbf0ac57
Hasan Naseem <hn92@live.com>
2018-05-28 17:31:15 -0400

    refactored some code and wrote tests for reset model

--- app/models/reset.rb
+++ app/models/reset.rb
@@ -4,24 +4,30 @@ class Reset < ApplicationRecord
 	validate :unique_token
 
 	def self.generate_reset_token(user)
-		@user = user
-		user_id = @user.id
+
+		user_id = user.id
 		token = SecureRandom.hex(10)
 		##create a new instance of a reset and save it to the matching user
 		reset = Reset.new(user_id: user_id, token: token)
 		reset.save
 		return token
-	end
 
+	end
 
 	def self.verify_user_requested_reset(token)
+
 		user = Reset.find_by(token: token)
+
 		if user
 			return User.find(user.user_id)
+
 		else
 			return false
+
 		end
+
 	end
+
 	def self.change_password(user, pass, passconf)
 		user.password = pass
 		user.password_confirmation = passconf
--- test/models/reset_test.rb
+++ test/models/reset_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ResetTest < ActiveSupport::TestCase
-	test 'user without token cannot reset password' do
+	def setup
 		user = User.new
 		user.first_name = 'test'
 		user.last_name = 'tester'
@@ -10,40 +10,30 @@ class ResetTest < ActiveSupport::TestCase
 		user.password_confirmation = 'pass'
 		user.match = true
 		user.save
+		user
+	end
 
+	test 'user without token cannot reset password' do
+		user = setup
 		reset = Reset.new(user_id: 2, token: "sfhgjfdjf8")
 		reset.save
 		assert_not_equal(user.id, reset.user_id)
 	end
 
 	test 'tokens are can be exhausted' do
-		user = User.new
-		user.first_name = 'test'
-		user.last_name = 'tester'
-		user.email = 'testemail'
-		user.password = 'pass'
-		user.password_confirmation = 'pass'
-		user.match = true
-		user.save
 
+		user = setup
 		reset1 = Reset.new(user_id: user.id, token: "sfhgjfdjf8")
 		reset1.save
 		reset1.token = "User Already Used This Token"
 		reset1.save
 
-		assert_equal(false, reset1.errors.any?)
+		assert_equal(true, reset1.errors.any?)
 	end
 
 	test 'tokens are unique' do
-		user = User.new
-		user.first_name = 'test'
-		user.last_name = 'tester'
-		user.email = 'testemail'
-		user.password = 'pass'
-		user.password_confirmation = 'pass'
-		user.match = true
-		user.save
 
+		user = setup
 		reset1 = Reset.new(user_id: user.id, token: "sfhgjfdjf8")
 		reset1.save
 		reset2  = Reset.new(user_id: user.id, token: "sfhgjfdjf8")
@@ -51,4 +41,25 @@ class ResetTest < ActiveSupport::TestCase
 
 		assert_equal(true, reset2.errors.any?)
 	end
+
+	test 'generate_reset_token method with valid user' do
+
+		user = setup
+		token = Reset.generate_reset_token(user)
+		assert token
+
+	end
+
+	test 'verify_user_requested_reset method' do
+
+		skip
+		user = setup
+		token = Reset.generate_reset_token(user)
+		reset = Reset.new(user_id: user.id, token: token)
+		reset.save
+		response = Reset.verify_user_requested_reset(token)
+		assert_equal(user, response)
+
+	end
+
 end
