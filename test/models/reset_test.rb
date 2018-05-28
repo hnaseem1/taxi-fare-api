eebb1addc85be0e3a653463505b19c3e1e706ce5
donrestarone <shashikejayatunge@gmail.com>
2018-05-27 20:34:19 -0400

    refactor Reset class custom validation for selective uniqueness and add tests to the said class

--- test/models/reset_test.rb
+++ test/models/reset_test.rb
@@ -15,4 +15,40 @@ class ResetTest < ActiveSupport::TestCase
 		reset.save
 		assert_not_equal(user.id, reset.user_id)
 	end
+
+	test 'tokens are can be exhausted' do
+		user = User.new
+		user.first_name = 'test'
+		user.last_name = 'tester'
+		user.email = 'testemail'
+		user.password = 'pass'
+		user.password_confirmation = 'pass'
+		user.match = true
+		user.save
+
+		reset1 = Reset.new(user_id: user.id, token: "sfhgjfdjf8")
+		reset1.save
+		reset1.token = "User Already Used This Token"
+		reset1.save
+
+		assert_equal(false, reset1.errors.any?)
+	end
+
+	test 'tokens are unique' do
+		user = User.new
+		user.first_name = 'test'
+		user.last_name = 'tester'
+		user.email = 'testemail'
+		user.password = 'pass'
+		user.password_confirmation = 'pass'
+		user.match = true
+		user.save
+
+		reset1 = Reset.new(user_id: user.id, token: "sfhgjfdjf8")
+		reset1.save
+		reset2  = Reset.new(user_id: user.id, token: "sfhgjfdjf8")
+		reset2.save
+
+		assert_equal(true, reset2.errors.any?)
+	end
 end
