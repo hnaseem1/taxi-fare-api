4b7d9e32b3d93cd6408a3860a6fc2c53460eecb5
donrestarone <shashikejayatunge@gmail.com>
2018-05-29 11:59:51 -0400

    start test

--- test/models/ride_test.rb
+++ test/models/ride_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class RideTest < ActiveSupport::TestCase
-	test 'cannot create ride without user' do
+	test 'cannot_create_ride_without_user' do
 		ride = Ride.new
 		ride.latitude_start = 12
 		ride.longitude_start = 1212
@@ -14,5 +14,11 @@ class RideTest < ActiveSupport::TestCase
 		ride.end_address = 'address'
 		ride.save
 		assert_equal(true, ride.errors.any?)
+	end
+
+	test 'return_start_location_favourite_for_user' do
+		user = User.new(first_name: 'bob', last_name: 'sagat', email: 'bob@sagat.com', password_digest: '12ewaglkfaer')
+		ride = Ride.new(latitude_start: 12, longitude_start: 12.4, latitude_end: 11, longitude_end: 14, provider: 'uber', price: 123.4, user_id: user.id, start_address: 'some place', end_address: 'narnia', start_favourite: true)
+		
 	end
 end
