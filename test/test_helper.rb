f2ad81bd90e9cde72ae917b3141edd83f2cb8b8b
donrestarone <shashikejayatunge@gmail.com>
2018-05-29 18:24:42 -0400

    fix test dependencies

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,17 @@
-# ENV['RAILS_ENV'] ||= 'test'
-# require_relative '../config/environment'
-# require 'rails/test_help'
-# require 'capybara/rails'
-# require 'capybara/minitest'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+require 'capybara/rails'
+require 'capybara/minitest'
 
-# class ActiveSupport::TestCase
-#   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-#   fixtures :all
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
 
-#   # Add more helper methods to be used by all tests here...
+  # Add more helper methods to be used by all tests here...
 
 
-# end
+end
 
 # class ActionDispatch::IntegrationTest
 #   # Make the Capybara DSL available in all integration tests
