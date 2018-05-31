b5c19f7a5c8243798802b31f94a33147098d2ed7
donrestarone <shashikejayatunge@gmail.com>
2018-05-31 13:24:21 -0400

    add controller test for reset redirect;

--- test/controllers/google_controller_test.rb
+++ test/controllers/google_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class GoogleControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get index" do
+  	get google_path
+  	assert_response :success
+  end
+
+  test "get root" do 
+  	get root_path 
+  	assert_response :success
+  end
 end
