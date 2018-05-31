d07a8b1154dd3d9b8ee32a82e1b463b20eeb544a
donrestarone <shashikejayatunge@gmail.com>
2018-05-31 13:39:48 -0400

    adjusted ride test a little

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,15 +7,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should get create" do
-    skip
+    
     post user_path(user: {first_name: 'test', last_name: 'testing', email: 'testmail', password: 'pass', password_confirmation: 'pass', match: true})
     assert_response :success
   end
-  
-  test "should get show" do
-    skip
-    get user_path
-    assert_response :success
-  end
-
 end
